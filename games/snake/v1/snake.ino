3c370a64a5d5018ad8da3ca64d3dad7f8c251c96
Dmytro Ageyev <3177816+dmageyev@users.noreply.github.com>
2026-03-14 16:14:40 +0200

    Implement basic snake movement and drawing

--- games/snake/v1/snake.ino
+++ games/snake/v1/snake.ino
@@ -1,10 +1,87 @@
 // Snake v1 - GameBoy (Arduino Nano)
 // Початкова версія гри "Змійка"
 
+#include <GameBoy.h>
+
+GameBoy gb;
+
+int snakeX[10];
+int snakeY[10];
+
+int up     = 0;
+int right  = 1;
+int bottom = 2;
+int left   = 3;
+int direction = right;
+
+int lenSnake = 5;
+const int numRows = 8;
+const int numCols = 16;
 void setup() {
-  // Ініціалізація дисплею, кнопок та інших периферійних пристроїв
+  gb.begin(0);
+  snakeX[0] = 4;
+  snakeY[0] = 7;
+
+}
+
+void makeMove() {
+  if (gb.getKey() == 4) {
+    direction = left;
+  }
+  if (gb.getKey() == 5) {
+    direction = right;
+  }
+  if (gb.getKey() == 6) {
+    direction = bottom;
+  }
+  if (gb.getKey() == 3) {
+    direction = up;
+  }
+}
+
+void move() {
+  for (int i = lenSnake - 1; i > 0; i--) {
+    snakeX[i] = snakeX[i - 1];
+    snakeY[i] = snakeY[i - 1];
+  }
+
+  if (direction == up) {
+    if (snakeY[0] == 0) {
+      snakeY[0] = 15;
+    } else {
+      snakeY[0]--;
+    }
+  } else if (direction == bottom) {
+    if (snakeY[0] == 15) {
+      snakeY[0] = 0;
+    } else {
+      snakeY[0]++;
+    }
+  } else if (direction == left) {
+    if (snakeX[0] == 0) {
+      snakeX[0] = 7;
+    } else {
+      snakeX[0]--;
+    }
+  } else if (direction == right) {
+    if (snakeX[0] == 7) {
+      snakeX[0] = 0;
+    } else {
+      snakeX[0]++;
+    }
+  }
+}
+
+void drawSnake() {
+  for (int i = 0; i < lenSnake; i++) {
+    gb.drawPoint(snakeX[i], snakeY[i]);
+  }
 }
 
 void loop() {
-  // Основний ігровий цикл
+  makeMove();
+  move();
+  gb.clearDisplay();
+  drawSnake();
+  delay(250);
 }

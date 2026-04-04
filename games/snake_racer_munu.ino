afe81eee89c734494604e5a56d6189a9d73531b0
Dmytro Ageyev <3177816+dmageyev@users.noreply.github.com>
2026-04-04 16:21:25 +0300

    Add Snake Racer game functionality
    Implement Snake Racer game with menu and gameplay mechanics

---
+++ games/snake_racer_munu.ino
@@ -0,0 +1,370 @@
+#include <GameBoy.h>
+
+GameBoy gb;
+
+//Menu var
+int modes = 0;
+int modeCount = 0;
+
+//CAR
+int enemy_x = 2;
+int car_speed = 100;
+int mycar_x = 2;
+int mycar_y = 12;
+volatile int line_y = 0;
+
+//SNAKE
+int snakeX[10];
+int snakeY[10];
+
+int up     = 0;
+int right  = 1;
+int bottom = 2;
+int left   = 3;
+int direction = right;
+
+int eatX, eatY;
+
+bool state;
+
+int lenSnake = 7;
+const int numRows = 8;
+const int numCols = 16;
+
+byte ARROWS[8][8] = {
+  { 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 1, 0, 0, 1, 0, 0 },
+  { 0, 1, 1, 0, 0, 1, 1, 0 },
+  { 1, 1, 1, 0, 0, 1, 1, 1 },
+  { 0, 1, 1, 0, 0, 1, 1, 0 },
+  { 0, 0, 1, 0, 0, 1, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0 }
+};
+
+
+byte CAR[8][8] = {
+  { 0, 1, 0, 1, 0, 0, 0, 0 },
+  { 0, 0, 1, 0, 0, 0, 0, 0 },
+  { 0, 1, 1, 1, 0, 0, 0, 0 },
+  { 0, 0, 1, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 1, 0, 0 },
+  { 0, 0, 0, 0, 1, 1, 1, 0 },
+  { 0, 0, 0, 0, 0, 1, 0, 0 },
+  { 0, 0, 0, 0, 1, 0, 1, 0 }
+};
+
+byte SNAKE[8][8] = {
+  { 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 1, 1, 1, 1, 0, 0 },
+  { 0, 1, 1, 0, 0, 1, 1, 0 },
+  { 0, 1, 1, 0, 0, 0, 0, 0 },
+  { 0, 0, 1, 1, 1, 1, 0, 0 },
+  { 0, 0, 0, 0, 0, 1, 1, 0 },
+  { 0, 1, 1, 0, 0, 1, 1, 0 },
+  { 0, 0, 1, 1, 1, 1, 0, 0 }
+};
+
+void setup() {
+  gb.begin(0);
+  randomSeed(analogRead(0) + analogRead(5));
+  //CAR
+  playerCar(mycar_x, mycar_y);
+  //SNAKE
+  snakeX[0] = 4;
+  snakeY[0] = 7;
+  makeFruit();
+
+}
+
+void mainMenu() {
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      gb.wipePoint(i, j);
+      gb.setLed(i, j, ARROWS[j][i]);
+    }
+  }
+if (modeSelector() == 0) {
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      gb.wipePoint(i, j +8);
+      gb.setLed(i, j +8, CAR[j][i]);
+    }
+  }
+}
+if (modeSelector() == 1) {
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      gb.wipePoint(i, j +8);
+      gb.setLed(i, j +8, SNAKE[j][i]);
+    }
+  }
+}
+
+}
+
+int modeSelector() {
+  if (gb.getKey() == 4) {
+    modeCount++;
+    delay(250);
+    if (modeCount > 1) {
+      modeCount = 0;
+    }
+  } else if (gb.getKey() == 5) {
+    modeCount--;
+    delay(250);
+    if (modeCount < 0) {
+      modeCount = 1;
+    }
+  }
+  return modeCount;
+}
+/*
+  
+*/
+
+void loop() {
+
+}
+
+void switchMode(int mode) {
+
+}
+
+
+
+
+
+
+void mainRaicing() 
+{
+  enemy_x = random(0, 10);
+  if (enemy_x > 5) enemy_x = 2;
+  else enemy_x = 5;
+
+  for (int enemy_y = 0; enemy_y < 16; enemy_y++) 
+  {
+    enemyCar(enemy_x, enemy_y);
+
+    createLine(enemy_y);
+    createLine(enemy_y + 5);
+    createLine(enemy_y + 10);
+    createLine(enemy_y + 15);
+    createLine(enemy_y - 5);
+    createLine(enemy_y - 10);
+    createLine(enemy_y - 15);
+
+    if (gb.getKey() == 4) {
+      clearPlayerCar(5, 12);
+      mycar_x = 2;
+    } else if (gb.getKey() == 5) {
+      clearPlayerCar(2, 12);
+      mycar_x = 5;
+    }
+    playerCar(mycar_x, mycar_y);
+
+    if (Collision(mycar_x, mycar_y, enemy_x, enemy_y)) {
+      gb.sound(COLLISION);
+      gb.testMatrix(10);
+      gb.clearDisplay();
+      return;
+    }
+    if (enemy_y>14)
+      gb.sound(SCORE);
+
+    delay(car_speed);
+
+    clearEnemyCar(enemy_x, enemy_y);
+
+    clearLine(enemy_y);
+    clearLine(enemy_y + 5);
+    clearLine(enemy_y + 10);
+    clearLine(enemy_y + 15);
+    clearLine(enemy_y - 5);
+    clearLine(enemy_y - 10);
+    clearLine(enemy_y - 15);
+  }
+}
+
+void enemyCar(int x, int y) {
+  gb.drawPoint(x, y);         
+  gb.drawPoint(x, y - 1);     
+  gb.drawPoint(x - 1, y - 1); 
+  gb.drawPoint(x + 1, y - 1); 
+  gb.drawPoint(x, y - 2);     
+  gb.drawPoint(x - 1, y - 3);
+  gb.drawPoint(x + 1, y - 3); 
+}
+
+void playerCar(int x, int y) {
+  gb.drawPoint(x, y);
+  gb.drawPoint(x, y + 1);
+  gb.drawPoint(x - 1, y + 1);
+  gb.drawPoint(x + 1, y + 1);
+  gb.drawPoint(x, y + 2);
+  gb.drawPoint(x - 1, y + 3);
+  gb.drawPoint(x + 1, y + 3);
+}
+
+void clearPlayerCar(int x, int y) {
+  gb.wipePoint(x, y);
+  gb.wipePoint(x, y + 1);
+  gb.wipePoint(x - 1, y + 1);
+  gb.wipePoint(x + 1, y + 1);
+  gb.wipePoint(x, y + 2);
+  gb.wipePoint(x - 1, y + 3);
+  gb.wipePoint(x + 1, y + 3);
+}
+
+void clearEnemyCar(int x, int y) {
+  gb.wipePoint(x, y);
+  gb.wipePoint(x, y - 1);
+  gb.wipePoint(x - 1, y - 1);
+  gb.wipePoint(x + 1, y - 1);
+  gb.wipePoint(x, y - 2);
+  gb.wipePoint(x - 1, y - 3);
+  gb.wipePoint(x + 1, y - 3);
+}
+
+void createLine(int y) {
+  gb.drawPoint(0, y);
+  gb.drawPoint(0, y + 1);
+  gb.drawPoint(0, y + 2);
+
+  gb.drawPoint(7, y);
+  gb.drawPoint(7, y + 1);
+  gb.drawPoint(7, y + 2);
+}
+
+void clearLine(int y) {
+  gb.wipePoint(0, y);
+  gb.wipePoint(0, y + 1);
+  gb.wipePoint(0, y + 2);
+
+  gb.wipePoint(7, y);
+  gb.wipePoint(7, y + 1);
+  gb.wipePoint(7, y + 2);
+}
+
+bool Collision(int p_x, int p_y, int e_x, int e_y) 
+{
+  if (p_x == e_x && p_y <= e_y) return true;
+  return false;
+}
+
+void makeMove() {
+  if (gb.getKey() == 4 && direction != right) {
+    direction = left;
+  }
+  if (gb.getKey() == 5 && direction != left) {
+    direction = right;
+  }
+  if (gb.getKey() == 6 && direction != up) {
+    direction = bottom;
+  }
+  if (gb.getKey() == 3 && direction != bottom) {
+    direction = up;
+  }
+}
+
+void move() {
+  if ((snakeX[0] == eatX) and (snakeY[0] == eatY)) {
+    lenSnake++;
+    makeFruit();
+    gb.sound(SCORE);
+  }
+  for (int i = lenSnake - 1; i > 0; i--) {
+    if (snakeX[0] == snakeX[i] && snakeY[0] == snakeY[i]) {
+      gb.sound(COLLISION);
+      loss();
+    }
+  }
+  for (int i = lenSnake - 1; i > 0; i--) {
+    snakeX[i] = snakeX[i - 1];
+    snakeY[i] = snakeY[i - 1];
+  }
+
+  if (direction == up) {
+    if (snakeY[0] == 0) {
+      snakeY[0] = 15;
+    } else {
+      snakeY[0]--;
+    }
+  } else if (direction == bottom) {
+    if (snakeY[0] == 15) {
+      snakeY[0] = 0;
+    } else {
+      snakeY[0]++;
+    }
+  } else if (direction == left) {
+    if (snakeX[0] == 0) {
+      snakeX[0] = 7;
+    } else {
+      snakeX[0]--;
+    }
+  } else if (direction == right) {
+    if (snakeX[0] == 7) {
+      snakeX[0] = 0;
+    } else {
+      snakeX[0]++;
+    }
+  }
+}
+
+void drawSnake() {
+  for (int i = 0; i < lenSnake; i++) {
+    gb.drawPoint(snakeX[i], snakeY[i]);
+  }
+}
+
+void drawFruit() {
+  state = !state;
+  if ( state == true) {
+    gb.drawPoint(eatX, eatY);
+  }
+  else {
+    gb.wipePoint(eatX, eatY);
+  }
+}
+boolean isPartOfSnake(int x, int y) {
+  for (int i = 0; i < lenSnake - 1; i++) {
+    if ((x == snakeX[i]) && (y == snakeY[i])) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void makeFruit() {
+  eatX = random(0, 8);
+  eatY = random(0, 16);
+  while (isPartOfSnake(eatX, eatY)) {
+
+    eatX = random(0, 8);
+    eatY = random(0, 16);
+  }
+}
+
+void loss() {
+  delay(1000);
+  gb.clearDisplay();
+  for (int i = 0; i < lenSnake; i++) {
+    snakeX[i] = 0;
+    snakeY[i] = 0;
+  }
+  direction = right;
+  eatX = 3;
+  eatY = 3;
+  lenSnake = 4;
+  snakeX[0] = 4;
+  snakeY[0] = 7;
+}
+
+void gameSnake() {
+  makeMove();
+  move();
+  gb.clearDisplay();
+  drawSnake();
+  drawFruit();
+  delay(250);
+}
